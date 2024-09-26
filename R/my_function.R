6e9be3492e0fcc812bd78090d7865d65909f91b0
wtao <1287420667@qq.com>
2024-09-26 15:39:36 +0800

    release:0.4.0

--- R/my_function.R
+++ R/my_function.R
@@ -7,9 +7,5 @@
 #' @examples：提供函数的使用示例代码。
 #' @details：进一步详细说明函数的工作机制和细节。
 #' @keywords：列出与函数相关的关键词，以便在文档中进行索引。
-#' @import：声明该函数依赖的外部包。
-#' @importFrom：声明该函数依赖的外部包中的特定函数。
-#' @importFrom rlang .data：引用数据框列名，避免 R CMD check 注意
-#' @export：表明函数将被导出，供用户使用。
-#' 
+#' @export
 a = function(x){x+1}
